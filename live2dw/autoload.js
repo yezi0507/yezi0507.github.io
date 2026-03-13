ff217d9fff26dae3ab1c45f0739c93514f6b6661
yezi0507 <2268486906@qq.com>
2026-03-14 04:30:25 +0800

    renovate liv2d show problem

--- live2dw/autoload.js
+++ live2dw/autoload.js
@@ -56,7 +56,7 @@ function loadExternalResource(url, type) {
   // For detailed usage of configuration options, see README.en.md
   // 配置选项的具体用法见 README.md
   initWidget({
-    waifuPath: live2d_path + 'waifu-tips.json',
+    waifuPath: '/live2dw/' + 'waifu-tips.json',
     // cdnPath: 'https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/',
     cubism2Path: live2d_path + 'live2d.min.js',
     cubism5Path: 'https://cubism.live2d.com/sdk-web/cubismcore/live2dcubismcore.min.js',
